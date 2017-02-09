cbd3e5aaac43e4090efb3f6b6037ca6928fd6314
shopf <shopf@PC10136532>
2017-02-09 12:53:34 +0100

    Änderung eins

--- Noobtest1/Noobtest1/Noobtest1.cpp
+++ Noobtest1/Noobtest1/Noobtest1.cpp
@@ -4,8 +4,24 @@
 #include <stdio.h>
 
 
-int main()
-{
-    return 0;
-}
+int main() {
 
+	int a = 0, b = 0, c = 0;
+
+	printf("Bitte Zahl eins Eingeben(n) ");
+	scanf("%i", &a);
+
+	printf("Sie haben bissher Eingegeben %i %i %i\n", a, b, c);
+
+	printf("Bitte Zahl zwei Eingeben(n) ");
+	scanf("%i", &b);
+
+	printf("Sie haben bissher Eingegeben %i %i %i\n", a, b, c);
+
+	printf("Bitte Zahl drei Eingeben(n) ");
+	scanf("%i", &c);
+
+	printf("Sie haben komplett Eingegeben %i %i %i \n", a, b, c);
+
+
+	return 0;
