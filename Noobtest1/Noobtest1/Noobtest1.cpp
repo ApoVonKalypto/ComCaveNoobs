49bdbb531605b01a6d24cb4279a946fd2088d1ea
Sascha <apokalypto@live.de>
2017-02-09 15:34:45 +0100

    hmmm noch ein sinnlostest

--- Noobtest1/Noobtest1/Noobtest1.cpp
+++ Noobtest1/Noobtest1/Noobtest1.cpp
@@ -6,8 +6,8 @@
 
 int main() {
 
-	int a = 0, b = 0, c = 0;
-
+	int a = 0, b = 0, c = 0, d = 0;
+	printf("Gib nacheinander 3 Zahlen ein\n");
 	printf("Bitte Zahl eins Eingeben(n) ");
 	scanf("%i", &a);
 
@@ -21,8 +21,10 @@ int main() {
 	printf("Bitte Zahl drei Eingeben(n) ");
 	scanf("%i", &c);
 
-	printf("Sie haben komplett Eingegeben %i %i %i \n", a, b, c);
-
+	printf("Sie haben komplett Eingegeben\n %i\n %i\n %i\n \n", a, b, c);
+	d = a + b + c;
+	printf("Addiert macht das: ");
+	printf(" %i \n \n", d);
 
 
 	return 0;
